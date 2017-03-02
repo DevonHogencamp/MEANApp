c88d1bd71b60efd7a88b615909768bf5f856158c
Devon Hogencamp <devhogen1@gmail.com>
2017-03-02 15:29:09 -0700

    we have a server

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+
+var app = express();
+
+var port = 8080;
+
+var server = app.listen(port, function () {
+    console.log('Server listening on localhost:' + port);
+});
