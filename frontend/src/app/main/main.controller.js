1e92d9d7ad949fff9555d5457b8184789e5ca1db
Devon Hogencamp <devhogen1@gmail.com>
2017-03-03 13:32:06 -0700

    added comments for college classes

--- frontend/src/app/main/main.controller.js
+++ frontend/src/app/main/main.controller.js
@@ -1,3 +1,11 @@
+/*
+    Author: Devon Hogencamp
+    Date: 3-2-17
+    main.controller.js
+
+    Purpose: Controller for main.html page
+*/
+
 export class MainController {
     constructor ($log) {
         'ngInject';
