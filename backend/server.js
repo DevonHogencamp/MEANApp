3841528d3fbae00f57b35780628f723a75ce0c1c
Devon Hogencamp <devhogen1@gmail.com>
2017-03-08 15:29:48 -0700

    debug this

--- backend/server.js
+++ backend/server.js
@@ -17,11 +17,15 @@ var app = express();
 
 var bodyParser = require('body-parser');
 
-var mongo = require('mongoose');
+var mongoose = require('mongoose');
 
 var port = 8080;
 
 var database;
+
+var Message = mongoose.model({
+    msg: String
+});
 
 app.use(bodyParser.json());
 
@@ -33,17 +37,26 @@ app.use(function (req, res, next) {
 
 app.post('/api/message', function (req, res) {
     console.log(req.body);
-    database.collection('msgs').insertOne(req.body);
+    // database.collection('msgs').insertOne(req.body);
+    var msg = new Message(req.body);
+
+    msg.save();
+
     res.status(200);
 });
 
-mongo.connect('mongodb://localhost:27017/test', function (err, db) {
-    if (!err) {
-        console.log('Connected to MongoDB');
+var mongoDB = 'mongodb://localhost:27017/test';
+mongoose.connect(mongoDB);
 
-        database = db;
-    }
-});
+var database = mongoose.connection;
+
+// mongo.connect('mongodb://localhost:27017/test', function (err, db) {
+//     if (!err) {
+//         console.log('Connected to MongoDB');
+//
+//         database = db;
+//     }
+// });
 
 var server = app.listen(port, function () {
     console.log('Server listening on localhost:' + port);
